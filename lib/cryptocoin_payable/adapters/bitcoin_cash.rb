69e96b40c1f3b693b1372602f5df9dd15aad3e52
Kingdon Barrett <kingdon@weave.works>
2022-05-01 20:13:17 -0400

    add bitcoin cash v1 api
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- lib/cryptocoin_payable/adapters/bitcoin_cash.rb
+++ lib/cryptocoin_payable/adapters/bitcoin_cash.rb
@@ -10,8 +10,26 @@ module CryptocoinPayable
       def fetch_transactions(address)
         raise NetworkNotSupported if CryptocoinPayable.configuration.testnet
 
-        url = "https://#{prefix}blockexplorer.com/api/txs/?address=#{legacy_address(address)}"
+        url = "https://explorer.api.bitcoin.com/bch/v1/txs?address=#{address}"
         parse_block_explorer_transactions(get_request(url).body, address)
+      end
+
+      def parse_block_explorer_transactions(response, address)
+        json = JSON.parse(response)
+        json['txs'].map { |tx| convert_explorer_api_transactions(tx, address) }
+      rescue JSON::ParserError
+        raise ApiError, response
+      end
+
+      def convert_explorer_api_transactions(transaction, address)
+        {
+          transaction_hash: transaction['txid'],
+          block_hash: transaction['blockhash'],
+          block_time: parse_timestamp(transaction['blocktime']),
+          estimated_time: parse_timestamp(transaction['time']),
+          estimated_value: parse_total_tx_value_block_explorer(transaction['vout'], address),
+          confirmations: transaction['confirmations']
+        }
       end
 
       def create_address(id)
